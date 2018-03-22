9fe2d4d74311993443a13351e029b018c87f3c77
yusefrich <yusef.rick@gmail.com>
2018-03-21 21:17:01 -0300

    First commit

---
+++ src/main/java/com/aulapablo/rev2/SpringWebApplication.java
@@ -0,0 +1,14 @@
+package com.aulapablo.rev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Sempre dar o play por aqui
+
+@SpringBootApplication
+public class SpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApplication.class, args);
+	}
+}
---
+++ src/main/java/com/aulapablo/rev2/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.aulapablo.rev2.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "Cat")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "restTestado";
+	}
+	
+}
